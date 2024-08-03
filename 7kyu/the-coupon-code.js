00002d7109496b9f18a0a89d2cd37a410187e910
Blake Jamison <BlaJam123@yahoo.com>
2024-08-02 23:14:35 -0400

    returns boolean of coupon being valid and not expired

---
+++ 7kyu/the-coupon-code.js
@@ -0,0 +1,57 @@
+/*Description:
+Story
+Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+Task
+Your mission:
+Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+Examples:
+checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+*/
+
+//=======================================================================================================================
+
+//My Solution:
+
+//PREP
+
+//P: enteredCode, correctCode, currentDate, expirationDate
+//R: return a boolean showing true if code is both valid and not expired
+//E: checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+//   checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  //convert date using Date() object
+  //conditionals: enteredCode === correctCode, currentDate <= expirationDate
+  //return booleen
+  const current = new Date(currentDate);
+  const expiration = new Date(expirationDate);
+  return enteredCode === correctCode && current <= expiration;
+}
+
+//==========================================================================================================================
+
+//Other Solutions:
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return (
+    enteredCode === correctCode &&
+    Date.parse(expirationDate) >= Date.parse(currentDate)
+  );
+}
+
+//==========================================================================================================================
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return (
+    enteredCode === correctCode &&
+    new Date(currentDate) <= new Date(expirationDate)
+  );
+}
+
+//==========================================================================================================================
