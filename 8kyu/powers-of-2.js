edea125982e5a12f73040c04c5fed1d95515e0fe
Blake Jamison <BlaJam123@yahoo.com>
2024-08-11 22:15:57 -0400

    add codewars solution

---
+++ 8kyu/powers-of-2.js
@@ -0,0 +1,36 @@
+/*
+
+Description:
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+*/
+
+// My Solution:
+
+function powersOfTwo(n) {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr.map((ele) => Math.pow(2, ele));
+}
+// Did not need to use two loops. arr.push(Math.pow(2, i)) instead of using map
+
+//===================================================================================================
+
+// Other Solutions:
+
+function powersOfTwo(n) {
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+//====================================================================================================
